3a6815a8644654b2988b98df133e756e5cd94800
alexander <605956261@qq.com>
2017-04-12 17:15:03 +0800

    .,.,.,

--- Page/buju.js
+++ Page/buju.js
@@ -1,34 +1,15 @@
-function getByClass(sClass){
-    var aResult=[];
-    var aEle=document.getElementsByTagName('*');
-
-    for(var i=0;i<aEle.length;i++){
-        if(aEle[i].className==sClass){
-            aResult.push(aEle[i]);
-        }
-    }
-    return aResult;
-}
 window.onload = function () {
     function move(direction, obj) {
         if (obj) {
             now = obj.index;
         }
-        for (var h = 0;h<oIm.length;h++){
-            oIm[h].style.width = 130+'px';
-            oIm[h].style.height = 80+'px';
-            oIm[h].style.marginTop = 20+'px';
-            oIm[h].style.opacity = 0.7;
-            oIm[h].style['-khtml-opacity'] = 0.7;
-            oIm[h].style['moz-opacity'] = 0.7;
-            oIm[h].style.fliter = 'alpha(Opacity=70)';
-            oIm[h].style['-webkit-filter']= 'blur(1px)';
-            oIm[h].style['-moz-filter']= 'blur(1px)';
-            oIm[h].style['-o-filter']= 'blur(1px)';
-            oIm[h].style['-ms-filter']= 'blur(1px)';
-            oIm[h].style.filter = 'blur(1px)';
-            text[h].style.display = 'none';
-        }
+        oIm
+            .css('width', 130+'px')
+            .css('height', 80+'px')
+            .css('marginTop',20+'px')
+            .css('opacity', 0.7)
+            .css('filter', 'blur('+1+'px)');
+        text.css('display','none');
         if (direction == 'pre'){
         now--;
         if (now<0){
@@ -40,34 +21,18 @@ window.onload = function () {
             now = 0;
         }
         }
-        text[now].style.display = 'block' ;
-        bigDiv[now].style.zIndex = nowIndex++;
-        back.style.backgroundImage = "url('img/"+now+".jpg')";
-        oIm[now].style.width = 150+'px';
-        oIm[now].style.height = 100+'px';
-        oIm[now].style.marginTop = 10+'px';
-        oIm[now].style.opacity = 1;
-        oIm[now].style['-khtml-opacity'] = 1;
-        oIm[now].style['moz-opacity'] = 1;
-        oIm[now].style.fliter = 'alpha(Opacity=100)';
-        oIm[now].style['-webkit-filter']= 'blur(0)';
-        oIm[now].style['-moz-filter']= 'blur(0)';
-        oIm[now].style['-o-filter']= 'blur(0)';
-        oIm[now].style['-ms-filter']= 'blur(0)';
-        oIm[now].style.filter = 'blur(0)';
+        text.eq(now).css('display','block');
+        $('.le img').attr('src','img/'+now+'.jpg');
+        $('#container').css('backgroundImage',"url('img/"+json[0].now+".jpg')");
+        oIm.eq(now)
+            .css('width', 150+'px')
+            .css('height', 100+'px')
+            .css('marginTop',10+'px')
+            .css('opacity', 1)
+            .css('filter', 'blur('+0+')');
     }
-    var back = document.getElementById('container');
-    var oDiv = document.getElementById('foot');
-    var oUl = oDiv.getElementsByTagName('ul')[0];
-    var oLi = oUl.getElementsByTagName('li');
-    var oIm = oUl.getElementsByTagName('img');
-    var oDiv2 = document.getElementById('central');
-    var bigDiv = getByClass('le');
-    var text = getByClass('text');
-    var bigIm = oDiv2.getElementsByTagName('img');
-    var next = document.getElementById('n');
-    var pre = document.getElementById('p');
-    var nowIndex = 2;
+    var oIm = $('#foot ul li img');
+    var text = $('.text');
     var now = 0;
     for (var i = 0;i<oIm.length;i++){
         oIm[i].index = i;
@@ -75,10 +40,56 @@ window.onload = function () {
             move(false,this)
         }
     }
-    pre.onclick = function () {
+    $('#p').click(function () {
         move('pre');
-    };
-    next.onclick = function () {
+    });
+    $('#n').click(function () {
         move('next');
-    }
+    })
 };
+
+//----------------------------------------大翻页--------------------------------------------------------
+$(function () {
+    var i = 1;
+    function ajax(obj, fun) {
+        $(obj).click(function () {
+            $(obj).attr('num',i);
+            if (obj == '#NEX'){
+                i+=5 ;
+            }else if(obj == '#PRE'){
+                i-=5;
+            }
+            $.ajax({
+                type: 'GET',
+                url: '../Cooperate/category.php',
+                dataType: 'json',
+                data: {
+                    'i': i,
+                    'm': 4,
+                    'id': 7,
+                    'num': 5
+                },
+                success:fun,
+                error: function () {
+                    alert('啊啊啊啊啊啊，出错了！')
+                }
+            })
+        })
+    }
+    var sImg = $('.img');
+    ajax('#PRE', function (response) {
+        // alert('http://localhost'+response[0].cateimg);
+        sImg.eq(0).attr('src', 'http://localhost'+response[0].cateimg);
+        sImg.eq(1).attr('src', 'http://localhost'+response[1].cateimg);
+        sImg.eq(2).attr('src', 'http://localhost'+response[2].cateimg);
+        sImg.eq(3).attr('src', 'http://localhost'+response[3].cateimg);
+        sImg.eq(4).attr('src', 'http://localhost'+response[4].cateimg);
+    });
+    ajax('#NEX', function (response) {
+        sImg.eq(0).attr('src','http://localhost'+response[0].cateimg);
+        sImg.eq(1).attr('src','http://localhost'+response[1].cateimg);
+        sImg.eq(2).attr('src','http://localhost'+response[2].cateimg);
+        sImg.eq(3).attr('src','http://localhost'+response[3].cateimg);
+        sImg.eq(4).attr('src','http://localhost'+response[4].cateimg);
+    });
+});
