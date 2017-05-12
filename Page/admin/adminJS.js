66013114d8760b9504ee3364547360e54786cc10
xiangtong chu <819022816@qq.com>
2017-05-13 01:27:37 +0800

    重写的后台管理页面

--- Page/admin/adminJS.js
+++ Page/admin/adminJS.js
@@ -1,44 +1,27 @@
 $(function () {
-    var studioSpan = $('#studio-manage-span');
-    var cateSpan = $('#cate-manage-span');
-    var productSpan = $('#product-manage-span');
-    var studManage = $('#studio-manage');
-    var cateManage = $('#cate-manage');
-    var proManage = $('#product-manage');
-    var studioChange = $('#studio-change1');
-    var arr = ['#admin-studio','#studio-list','#studio-info','#studio-add','#admin-product-cate',
-        '#cate-list','#cate-add','#admin-product','#product-list','#product-info','#producti-add'];
-    // studiomanage.on('click',function () {
-    //     $('#studio-dropmenu-ul')
-    //
-    // });
-    studioSpan.click(function () {
-        $('#studio-dropmenu-ul').slideToggle();
-    });
-    cateSpan.click(function () {
-        $('#cate-dropmenu-ul').slideToggle();
-    });
-    productSpan.click(function () {
-        $('#product-dropmenu-ul').slideToggle();
-    });
-    studManage.click(function () {
-        $('#studio-list').css('display','block');
-    });
-    studioChange.click(function () {
-        $(arr[0]).css('display','block');
-        $(arr[3]).css('display','block');
-        $(arr[1]).css('display','none');
-        $(arr[2]).css('display','none');
-        $(arr[4]).css('display','none');
-        $(arr[7]).css('display','none');
-        $('#studio-change-check').css('display','block');
-        $('#studio-add-check').css('display','none');
-        // for ( var i=0;;i++){
-        //     if((i!=0)&&(i!=2)){
-        //         $(arr[i]).css('display','none');
-        //     }
-        // }
 
+    // 全部工作室
+    $('#all-studio').on('click',function () {
+        $('#studio-list').css('display','block');
+        $('#')
     });
+    // 添加工作室
+   $('#add-studio').click(function () {
+       $('.right-first-list').css('display','none');
+       $('#studio-add').css('display','block');
+       $('#studio-info').css('display','none');
+       $('#studio-add-check').css('display','block');
+       $('#studio-change-check').css('display','none');
+   });
+
+   // 修改工作室信息
+   $('#studio-change').click(function () {
+       $('.right-first-list').css('display','none');
+       $('#studio-add').css('display','block');
+       $('#studio-info').css('display','none');
+       $('#studio-add-check').css('display','none');
+       $('#studio-change-check').css('display','block');
+   })
 
 });
+
