e6023ae2381d6f212010d00d5865347d08998150
alexander <605956261@qq.com>
2017-05-17 23:07:51 +0800

    aaaaaa

--- Page/js/index.js
+++ Page/js/index.js
@@ -7,5 +7,28 @@ $(function(){
     setInterval(function(){
         angle += 0.2;
         earth.rotate(angle);
-    },40)
+    },40);
+});
+$(function(){
+    var oDiv = document.getElementById('Move');
+    var oUl = oDiv.getElementsByTagName('ul')[0];
+    var oLi = oDiv.getElementsByTagName('li');
+    oUl.innerHTML +=oUl.innerHTML;
+    oUl.style.height = oLi[0].offsetHeight*oLi.length+'px';
+    function move () {
+//            if (oUl.offsetTop<-oUl.offsetHeight/2){
+//                oUl.style.Top = '0';
+//            }
+        if (oUl.offsetTop > 0){
+            oUl.style.top = -oUl.offsetHeight/2+'px';
+        }
+        oUl.style.top = oUl.offsetTop+2+'px';
+    }
+    var timer = setInterval(move,30);
+    oDiv.onmouseover = function(){
+        clearInterval(timer);
+    };
+    oDiv.onmouseout = function(){
+        timer = setInterval(move,30);
+    }
 });
