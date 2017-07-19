7d09d97681b92f2a27c0053b1b5f999c941e4fd7
cxt <819022816@qq.com>
2017-07-19 15:37:10 +0800

    重写的后台管理页面

---
+++ Page/admin/js/adminJS.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('#we_studio').on('click',function () {
+        $('#studio_toggle').toggle(1000);
+        $('#we_studio span').toggle();
+    });
+    $('#we_product').on('click',function () {
+        $('#product_toggle').toggle(1000);
+        $('#we_product span').toggle();
+    });
+    $('#classify').on('click',function () {
+        $('#classify_toggle').toggle(1000);
+        $('#classify span').toggle();
+    });
+    $('#operation').on('click',function () {
+        $('#operation_toggle').toggle(1000);
+        $('#operation span').toggle();
+    })
+});
+
