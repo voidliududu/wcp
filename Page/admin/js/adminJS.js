2ead724e79ce09f6887cabf94cf1772b6efcde05
firstrain <824905239@qq.com>
2017-09-11 15:58:33 +0800

    aaaaaa

--- Page/admin/js/adminJS.js
+++ Page/admin/js/adminJS.js
@@ -1,19 +1,19 @@
 $(function () {
     // 收起展开列表
     $('#we_studio').on('click',function () {
-        $('#studio_toggle').toggle(1000);
+        $('#studio_toggle').toggle(500);
         $('#we_studio span').toggle();
     });
     $('#we_product').on('click',function () {
-        $('#product_toggle').toggle(1000);
+        $('#product_toggle').toggle(500);
         $('#we_product span').toggle();
     });
     $('#classify').on('click',function () {
-        $('#classify_toggle').toggle(1000);
+        $('#classify_toggle').toggle(500);
         $('#classify span').toggle();
     });
     $('#operation').on('click',function () {
-        $('#operation_toggle').toggle(1000);
+        $('#operation_toggle').toggle(500);
         $('#operation span').toggle();
     });
 
@@ -23,7 +23,7 @@ $(function () {
         stdioIntro = $(children[3]).val();
         stdioBranch = $(children[4]).children('.add_input').val();
         $.ajax({
-            url : 'http://localhost/wcp/Cooperate/dept.php?m=1',
+            url : 'http://localhost/wcp/Cooperate//dept.php?m=1',
             data : {
                 name : stdioName ,
                 brand : '' ,
--- Page/admin/js/adminJS_display.js
+++ Page/admin/js/adminJS_display.js
@@ -1,4 +1,17 @@
-
+function listColor_change(id){ //5eacd2
+    $('#all_product').css('background-color','#425671');
+    $('#all_studio').css('background-color','#425671');
+    $('#add_studio').css('background-color','#425671');
+    $('#add_product').css('background-color','#425671');
+    $('#we_movie').css('background-color','#425671');
+    $('#we_app').css('background-color','#425671');
+    $('#we_cartoon').css('background-color','#425671');
+    $('#we_mv').css('background-color','#425671');
+    $('#we_magazine').css('background-color','#425671');
+    $('#we_activity').css('background-color','#425671');
+    //$('#we_vote').css('background-color','#425671');
+    $('#'+id).css('background-color','#5eacd2');
+}
 // 功能切换
 $(function () {
     // 所有工作室
@@ -15,16 +28,9 @@ $(function () {
         $('.product_info_page').css('display','none');
         $('.studio_info_page').css('display','none');
 
-
-
         // 列表选项背景色
         $('#index').removeClass('active');
-        $('#all_studio').css('background-color','#5eacd2');
-        $('#add_studio').css('background-color','#425671');
-        $('#all_product').css('background-color','#425671');
-        $('#add_product').css('background-color','#425671');
-
-
+        listColor_change('all_studio');
     });
 
     // 所有微产品
@@ -43,11 +49,37 @@ $(function () {
 
         // 列表选项背景色
         $('#func_intro').removeClass('active');
-        $('#all_product').css('background-color','#5eacd2');
-        $('#all_studio').css('background-color','#425671');
-        $('#add_studio').css('background-color','#425671');
-        $('#add_product').css('background-color','#425671');
+        listColor_change('all_product');
+    });
 
+    //微电影
+    $('#we_movie').on('click',function () {
+        listColor_change('we_movie');
+    });
+
+    //app
+    $('#we_app').on('click',function () {
+        listColor_change('we_app');
+    });
+
+    //微漫画
+    $('#we_cartoon').on('click',function () {
+        listColor_change('we_cartoon');
+    });
+
+    //音乐MV
+    $('#we_mv').on('click',function () {
+        listColor_change('we_mv');
+    });
+
+    //微杂志
+    $('#we_magazine').on('click',function () {
+        listColor_change('we_magazine');
+    });
+
+    //微活动
+    $('#we_activity').on('click',function () {
+        listColor_change('we_activity');
     });
 
     // 添加工作室
@@ -73,10 +105,8 @@ $(function () {
 
         // 列表选项背景色
         $('#func_intro').removeClass('active');
-        $('#add_studio').css('background-color','#5eacd2');
-        $('#all_studio').css('background-color','#425671');
-        $('#all_product').css('background-color','#425671');
-        $('#add_product').css('background-color','#425671');
+        listColor_change('add_studio');
+
 
     });
 
@@ -103,15 +133,13 @@ $(function () {
 
         // 列表选项背景色
         $('#func_intro').removeClass('active');
-        $('#add_product').css('background-color','#5eacd2');
-        $('#add_studio').css('background-color','#425671');
-        $('#all_studio').css('background-color','#425671');
-        $('#all_product').css('background-color','#425671');
+        listColor_change('add_product');
+
 
     });
 
     // 修改工作室信息
-    $('#change1').on('click',function () {
+    $('.stu_change').on('click',function () {
         // 页面显隐
         $('#add_studio_page').css('display','block');
 
@@ -125,10 +153,16 @@ $(function () {
         $('#studio_add_check').css('display','none');
         $('.add_studio').css('display','none');
 
+        stu_name = $(this).prev().prev().text();
+        stu_branch = $(this).prev().text();
+        // $.ajax({
+        //     url : ''
+        //
+        // })
     });
 
     // 修改微产品信息
-    $('#pro_change1').on('click',function () {
+    $('.pro_change').on('click',function () {
         // 页面显隐
         $('#add_product_page').css('display','block');
 
@@ -141,10 +175,14 @@ $(function () {
 
         $('#product_add_check').css('display','none');
         $('.add_product').css('display','none');
+
+        pro_name = $(this).prev().prev().prev().text();
+        pro_branch = $(this).prev().prev().text();
+        pro_type = $(this).prev().text();
     });
 
     // 查看工作室信息
-    $('#studio1').on('click',function () {
+    $('.stu_detail').on('click',function () {
         // 页面显隐
         $('#studio1_info').css('display','block');
 
@@ -159,7 +197,7 @@ $(function () {
     });
 
     // 查看微产品信息
-    $('#product1').on('click',function () {
+    $('.pro_detail').on('click',function () {
         // 页面显隐
         $('#product1_info').css('display','block');
 
@@ -172,4 +210,22 @@ $(function () {
         $('#product_change_check').css('display','none');
 
     })
+
+    // 删除工作室信息
+    $('.stu_delete').on('click',function () {
+       if(confirm("你确定要删除吗？")){
+           if(confirm("删除将会无法复原，你真的要删除吗？")){
+                
+           }
+       }
+    });
+
+    // 删除微产品信息
+    $('.pro_delete').on('click',function () {
+        if(confirm("你确定要删除吗？")){
+            if(confirm("删除将会无法复原，你真的要删除吗？")){
+
+            }
+        }
+    });
 });
